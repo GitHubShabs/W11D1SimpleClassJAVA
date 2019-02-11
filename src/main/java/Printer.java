120eaba674cebc6e22e2b74002aa271a880cef1f
Shabana Mirza <shabanamirza2010@hotmail.com>
2019-02-11 17:13:54 +0000

    Java Lab/homework, extension complete

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,2 +1,44 @@
 public class Printer {
+
+    private int numPaperLeft;
+    private int toner;
+
+
+
+    public Printer(int numPaperLeft, int toner){
+        this.numPaperLeft = numPaperLeft;
+        this.toner = toner;
+
+
+    }
+
+    public int Print(int numPages, int numCopies) {
+        int numPaperUsed = (numPages * numCopies);
+        if (this.numPaperLeft >= numPaperUsed) {
+            this.numPaperLeft = this.numPaperLeft - numPaperUsed;
+            this.toner = this.toner - numPaperUsed;
+            return this.numPaperLeft;
+
+        }else{
+            this.refil(numPaperUsed);
+            return this.numPaperLeft;
+        }
+    }
+
+
+
+    public void refil(int numPaper){
+        this.numPaperLeft = this.numPaperLeft + numPaper;
+    }
+
+
+    public int getToner(){
+        return this.toner;
+    }
+
+
+
+
+
+
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,2 +1,29 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PrinterTest {
+
+    @Test
+    public void printerCheck(){
+        Printer printer = new Printer(6, 15);
+        assertEquals(0, printer.Print(1, 6));
+    }
+
+    @Test
+    public void addingPaper(){
+        Printer printer = new Printer(5, 15);
+        assertEquals(11 , printer.Print(1, 6));
+    }
+
+    @Test
+    public void tonerCheck(){
+        Printer printer = new Printer(6, 15);
+        printer.Print(1, 6);
+        assertEquals(9, printer.getToner());
+    }
+
+
+
+
 }
