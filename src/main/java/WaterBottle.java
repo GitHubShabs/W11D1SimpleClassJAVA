f27a40f57dfbc6ccc800a1ecac244128090fce6f
Shabana Mirza <shabanamirza2010@hotmail.com>
2019-02-11 15:37:43 +0000

    waterbottle tests completed

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -9,12 +9,19 @@ public class WaterBottle {
   public int drink(){
     int result = this.volume - 10;
      return result;
-
   }
 
+    public int emptyDrink(){
+        int result = this.volume = 0;
+        return result;
+    }
 
-
-
+    public int fullDrink(){
+        int currentValue = this.volume;
+        int topUp = 100 - currentValue;
+        int result = currentValue + topUp;
+        return result;
+    }
 
 
 
--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -8,12 +8,19 @@ public class WaterBottleTest {
     public void addDrink(){
         WaterBottle waterbottle = new WaterBottle(100);
         assertEquals(90, waterbottle.drink());
-
     }
 
-    
+    @Test
+    public void emptyDrink(){
+        WaterBottle waterbottle = new WaterBottle(100);
+        assertEquals(0, waterbottle.emptyDrink());
+    }
 
-
+    @Test
+    public void topUpDrink(){
+        WaterBottle waterbottle = new WaterBottle(60);
+        assertEquals(100, waterbottle.fullDrink());
+    }
 
 
 
